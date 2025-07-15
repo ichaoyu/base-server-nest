ad47cd41db7a4033e677044e4acd3eb918a09f48
chaoyu <chaoyumail@126.com>
2025-07-15 17:00:45 +0800

    feat: 新增参数管道校验

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
+import { APP_INTERCEPTOR, APP_PIPE, APP_FILTER } from '@nestjs/core';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { WinstonModule } from 'nest-winston';
@@ -9,6 +9,7 @@ import { LOGGER_OPTIONS } from './utils';
 import { CommonModule } from './apis/common';
 import { ApiModule } from './apis';
 import { ResponseInterceptor } from './interceptors';
+import { ValidationPipe } from './pipes';
 import {
 	DefaultExceptionFilter,
 	NotFoundExceptionFilter,
@@ -37,6 +38,10 @@ import {
 		{
 			provide: APP_INTERCEPTOR,
 			useClass: ResponseInterceptor,
+		},
+		{
+			provide: APP_PIPE,
+			useClass: ValidationPipe,
 		},
 		{
 			provide: APP_FILTER,
---
+++ src/pipes/validation.pipe.ts
@@ -0,0 +1,33 @@
+import { MESSAGES } from '@/constants';
+import {
+	Injectable,
+	PipeTransform,
+	ArgumentMetadata,
+	UnprocessableEntityException,
+} from '@nestjs/common';
+import { plainToInstance } from 'class-transformer';
+import { validate } from 'class-validator';
+
+// 参数管道校验
+@Injectable()
+export class ValidationPipe implements PipeTransform<any> {
+	async transform(value: any, { metatype }: ArgumentMetadata) {
+		if (!metatype || !this.toValidate(metatype)) {
+			return value;
+		}
+		const object = plainToInstance(metatype, value);
+		const errors = await validate(object, { stopAtFirstError: true });
+		if (errors.length > 0) {
+			const errorMsg = errors[0]?.constraints
+				? Object.values(errors[0].constraints)[0]
+				: MESSAGES.UNPROCESSABLE_ENTITY;
+
+			throw new UnprocessableEntityException(errorMsg);
+		}
+	}
+
+	private toValidate(metatype: any): boolean {
+		const types = [String, Boolean, Number, Array, Object];
+		return !types.includes(metatype);
+	}
+}
